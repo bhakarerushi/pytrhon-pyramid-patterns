c2cfedf803cd12480969b96501d4a642bd240984
Rushi Bhakare <bhakarerushi1996@gmail.com>
2021-02-11 10:31:34 +0530

    Connected Inverted pyramid pattern of Numbers .

--- Python_Pettrn.py
+++ Python_Pettrn.py
@@ -1,18 +1,19 @@
 
-# unique pyramid patterns of digits.
+# Connected Inverted pyramid pattern of Numbers .
 
 n = int(input("enter no:"))
 
+for i in range(0,n+1):
 
-for i in range(1,n+1):
+    for j in range(n,i,-1):
+        print(j,end = ' ')
 
-    for j in range(1,i):
-
-        print(j, end = ' ')
+    for I in range(i):
+        print('', end = '')
     
-    for j in range(i,0,-1):
-
-        print(j, end = ' ' )
+    for k in range(i+1,n+1):
+        print(k,end= ' ')
+    
 
     print(' ')
 
@@ -20,10 +21,10 @@ for i in range(1,n+1):
 '''
 
 enter no:5
-1
-1 2 1
-1 2 3 2 1
-1 2 3 4 3 2 1
-1 2 3 4 5 4 3 2 1
+5 4 3 2 1 1 2 3 4 5
+5 4 3 2 2 3 4 5
+5 4 3 3 4 5
+5 4 4 5
+5 5
 
 '''
