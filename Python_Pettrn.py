98a9b07c2e0d644b765723090dfc7a3875549265
Rushi Bhakare <bhakarerushi1996@gmail.com>
2021-02-11 11:22:22 +0530

    Inverted Equilateral triangle pyramid with star(*)

--- Python_Pettrn.py
+++ Python_Pettrn.py
@@ -1,19 +1,19 @@
 
-# Equilateral triangle with star (*) symbol.
+#  Inverted Equilateral triangle with star (*) symbol.
 
 n = int(input("enter no:"))
 
 m = (2 * n) - 2
-for i in range(1,n+1):
+for i in range(n,-1,-1):
 
     for j in range(0,m):
 
         print(end = ' ')
     
-    m = m-1
+    m = m+1
 
 
-    for j in range(1,i+1):
+    for j in range(i,0,-1):
         print('*', end=' ')
 
     print(' ')
@@ -23,13 +23,15 @@ for i in range(1,n+1):
 '''
 
 output : 
-enter no:5
-        *
-       * *
-      * * *
-     * * * *
-    * * * * *
-
-
+enter no:10
+                  * * * * * * * * * *  
+                   * * * * * * * * *
+                    * * * * * * * *
+                     * * * * * * *
+                      * * * * * *
+                       * * * * *
+                        * * * *
+                         * * *
+                          * *
 
 '''
