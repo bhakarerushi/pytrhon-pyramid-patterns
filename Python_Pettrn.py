858ec964a6cd6311d7494ac8f0c799282f3d5721
Rushi Bhakare <bhakarerushi1996@gmail.com>
2021-02-11 08:15:11 +0530

    half inverted pyramid of same digit.

--- Python_Pettrn.py
+++ Python_Pettrn.py
@@ -1,23 +1,27 @@
 
-# half pyramid pattern of descending numbers.
+# half inverted pyramid of same digit.
 
 
 n = int(input("enter no:"))
+
 
 for i in range(n,0,-1):
 
     for j in range(i):
 
-        print(i,end = " ")
+        print(n,end = ' ')
+
     print(" ")
 
 
+
 '''
+
 enter no:5
 5 5 5 5 5
-4 4 4 4
-3 3 3
-2 2
-1
+5 5 5 5
+5 5 5
+5 5
+5
 
 '''
