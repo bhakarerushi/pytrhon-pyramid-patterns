ca9cc43d494e2807ed0ff4dbee1388e7dfcd8829
Rushi Bhakare <bhakarerushi1996@gmail.com>
2021-02-11 08:06:40 +0530

    half pyramid patern with mix numbers.

--- Python_Pettrn.py
+++ Python_Pettrn.py
@@ -1,10 +1,24 @@
 
-#inverted pyramid pattern
+# half pyramid patern with mix numbers.
 
 n  = int(input("enter no:"))
 
-for i in range(n,0,-1):
-    for j in range(i):
+for i in range(1,n+1):
 
-        print(i,end= '')
-    print(' ')
+    for j in range(1,i+1):
+
+        print(j,end = " ")
+    print('')
+
+'''
+
+output:
+
+enter no:5
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+'''
