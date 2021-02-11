ccf051e7896abd4a01871a7101fcb90a9b6423d9
Rushi Bhakare <bhakarerushi1996@gmail.com>
2021-02-11 08:27:56 +0530

    inverted half pyramid of numbers starting with 0.

--- Python_Pettrn.py
+++ Python_Pettrn.py
@@ -1,23 +1,24 @@
 
-# reverse pyramid of numbers.
+# inverted half pyramid of numbers starting with 0.
 
 n = int(input("enter no:"))
 
+for i in range(n,0,-1):
 
-for i in range(1,n+1):
+    for j in range(i+1):
 
-    for j in range(i,0,-1):
-        print(j,end = " ")
+        print(j,end = ' ')
 
     print(" ")
+
 
 '''
 
 enter no:5
-1
-2 1
-3 2 1
-4 3 2 1
-5 4 3 2 1
+0 1 2 3 4 5
+0 1 2 3 4
+0 1 2 3
+0 1 2
+0 1
 
 '''
